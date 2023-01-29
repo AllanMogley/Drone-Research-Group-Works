387e6a5756aba70fb36d92800d0c2e1e311b2d47
Allan Mogley <allanmogley@gmail.com>
2023-01-29 23:30:06 +0300

    Juja Landsat 8 Overpass predictor

---
+++ Project SpaceTrack/Code/SGP4 Model.py
@@ -0,0 +1,29 @@
+import datetime
+from orbit_predictor.sources import EtcTLESource
+# from orbit_predictor.locations import JUJA
+from orbit_predictor import locations
+from orbit_predictor.sources import get_predictor_from_tle_lines
+
+
+# Satelitte TLE values (Landsat 8)
+# ----------------------------------------------------------------------------------
+TLE_LINES = (
+            "1 39084U 13008A   23029.41611366  .00000452  00000+0  11041-3 0  9990",
+            "2 39084  98.1979 101.5520 0001124  81.3714 278.7612 14.57109848529900")
+
+predictor = get_predictor_from_tle_lines(TLE_LINES)
+
+
+# My Location of interest
+# ----------------------------------------------------------------------------------
+JUJA = locations.Location("JUJA", latitude_deg=-1.1018, longitude_deg=37.0144,
+                            elevation_m=1519)
+
+
+# Predict Next Pass
+# ----------------------------------------------------------------------------------
+next_pass = datetime.datetime.utcnow() + datetime.timedelta(days=17)
+print(predictor.get_next_pass(JUJA, next_pass, max_elevation_gt=1519))
+# print(predictor.get_position(datetime.datetime(2019, 1, 1)))
+
+
