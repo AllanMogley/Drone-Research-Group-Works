f258c9cf8f11b241fab5e0dcaf7f88fdc558b425
Allan Mogley <120258354+AllanMogley@users.noreply.github.com>
2023-02-01 17:10:29 +0300

    Deleted unused TLEs

--- Project SpaceTrack/Code/SGP4 Model.py
+++ Project SpaceTrack/Code/SGP4 Model.py
@@ -5,14 +5,6 @@ from orbit_predictor import locations
 from orbit_predictor.sources import get_predictor_from_tle_lines
 
 
-# Satelitte TLE values (Landsat 8)
-# ----------------------------------------------------------------------------------
-TLE_LINES = (
-            "1 39084U 13008A   23029.41611366  .00000452  00000+0  11041-3 0  9990",
-            "2 39084  98.1979 101.5520 0001124  81.3714 278.7612 14.57109848529900")
-
-
-# print(dir(TLEs))
 predictor = get_predictor_from_tle_lines(TLEs.S2A)
 
 
@@ -24,6 +16,6 @@ JUJA = locations.Location("JUJA", latitude_deg=-1.1018, longitude_deg=37.0144,
 
 # Predict Next Pass
 # ----------------------------------------------------------------------------------
-next_pass = datetime.datetime.utcnow() + datetime.timedelta(days=0)
+next_pass = datetime.datetime.utcnow() + datetime.timedelta(days=1)
 print(predictor.get_next_pass(JUJA, next_pass, max_elevation_gt=30))
 # print(predictor.get_position(datetime.datetime(2019, 1, 1))).
