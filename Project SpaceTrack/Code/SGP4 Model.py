adf43251152fe75397c67e22ccf6c0976cdff44a
Allan Mogley <120258354+AllanMogley@users.noreply.github.com>
2023-02-02 16:38:34 +0300

    Edited Day Durations

--- Project SpaceTrack/Code/SGP4 Model.py
+++ Project SpaceTrack/Code/SGP4 Model.py
@@ -5,7 +5,7 @@ from orbit_predictor import locations
 from orbit_predictor.sources import get_predictor_from_tle_lines
 
 
-predictor = get_predictor_from_tle_lines(TLEs.S2A)
+predictor = get_predictor_from_tle_lines(TLEs.LS8)
 
 
 # My Location of interest
@@ -16,6 +16,6 @@ JUJA = locations.Location("JUJA", latitude_deg=-1.1018, longitude_deg=37.0144,
 
 # Predict Next Pass
 # ----------------------------------------------------------------------------------
-next_pass = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+next_pass = datetime.datetime.utcnow() + datetime.timedelta(days=-8)
 print(predictor.get_next_pass(JUJA, next_pass, max_elevation_gt=30))
 # print(predictor.get_position(datetime.datetime(2019, 1, 1))).
