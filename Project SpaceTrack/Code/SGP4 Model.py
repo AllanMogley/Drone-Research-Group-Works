de1d5661d608a210f0cff6d6101691d3e637c18f
Allan Mogley <120258354+AllanMogley@users.noreply.github.com>
2023-02-13 17:19:59 +0300

    Edited Prediction Range

--- Project SpaceTrack/Code/SGP4 Model.py
+++ Project SpaceTrack/Code/SGP4 Model.py
@@ -13,39 +13,40 @@ JUJA = locations.Location("JUJA", latitude_deg=-1.1018, longitude_deg=37.0144,
 # ----------------------------------------------------------------------------------
 
 
-predictor = get_predictor_from_tle_lines(TLEs.LS8)
 
+predictor = get_predictor_from_tle_lines(TLEs.LS8)
 
 def satelitte():
     if predictor.sate_id == 39084:
         n = 16
+        print("\nLANDSAT 8")
         return n
-        print("LANDSAT 8")
 
     elif predictor.sate_id == 39634:
         n = 24
+        print("\nSENTINEL 1A")
         return n
-        print("SENTINEL 1A")
 
     elif predictor.sate_id == 41335:
         n = 24
+        print("\nSENTINEL 1B")
         return n
-        print("SENTINEL 1B")
 
     elif predictor.sate_id == 40697:
         n = 10
+        print("\nSENTINEL 2A")
         return n
-        print("SENTINEL 2A")
 
 
 
 # Predict Next Pass
 # ----------------------------------------------------------------------------------
-x = range(0, (satelitte())*6, satelitte())
+x = range(0, (satelitte())*5, satelitte())
+print("\n\n")
 for i in x:
-    next_pass = datetime.datetime(2022, 12, 29) + datetime.timedelta(days = i)
+    next_pass = datetime.datetime(2022, 12, 10) + datetime.timedelta(days = i)
+    print("Day", i)
     print(predictor.get_next_pass(JUJA, next_pass, max_elevation_gt=30))
-    print(i)
 
 
 # print(predictor.get_position(datetime.datetime(2022, 11, 24)))
