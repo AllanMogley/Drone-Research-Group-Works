57df995bd9a738cd4f1007e1f55529257ed55063
Allan Mogley <allanmogley@gmail.com>
2023-07-13 00:33:57 +0300

    Added USGS APIs

--- Project SpaceTrack/APIs/Data Browse.py
+++ Project SpaceTrack/APIs/Data Browse.py
@@ -22,18 +22,22 @@ scenes = api.search(
     latitude=-1.1018,
     longitude=37.0144,
     start_date='2023-01-01',
-    end_date='2023-12',
-    max_cloud_cover=50
+    end_date='2023-12'
+    # max_cloud_cover=90
 )
 
 print(f"{len(scenes)} scenes found.")
 
 # Process the result
+passes = []  # Create an empty list
 for scene in scenes:
     print(scene['acquisition_date'].strftime('%Y-%m-%d'))
-    # Write scene footprints to disk
-    fname = f"{scene['landsat_product_id']}.geojson"
-    with open(fname, "w") as f:
-        json.dump(scene['spatial_coverage'].__geo_interface__, f)
+    # passes.append(scene)  Fill List with LS8/9 PassDates
 
+
+    # Export scene footprints to disk
+    # fname = f"{scene['landsat_product_id']}.geojson"
+    # with open(fname, "w") as f:
+    #     json.dump(scene['spatial_coverage'].__geo_interface__, f)
+# print(passes)
 api.logout()
