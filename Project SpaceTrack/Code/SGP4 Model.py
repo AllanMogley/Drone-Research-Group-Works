72e45c25b4258c9acbf6f7aa0a9d134c86134c10
Allan Mogley <allanmogley@gmail.com>
2023-07-31 23:56:42 +0300

    Cleaned the Code

--- Project SpaceTrack/Code/SGP4 Model.py
+++ Project SpaceTrack/Code/SGP4 Model.py
@@ -19,9 +19,10 @@ JUJA = locations.Location("JUJA", latitude_deg=-1.1018, longitude_deg=37.0144,
 # ----------------------------------------------------------------------------------
 predictor2 = get_predictor_from_tle_lines(TLEs.S2A)
 predictor = get_predictor_from_tle_lines(TLEs.LS8)
+
 def satelitte():
     if predictor.sate_id == 39084:
-        # print("LANDSAT 8 & 7")
+        # print("LANDSAT 8")
         n = 8
         return n
 
@@ -49,26 +50,17 @@ x = range(0, (satelitte())*11, satelitte()) # type: ignore
 print("\n\n")
 print("LANDSAT 8")
 print("TODAYS DATE", T.date())
-print("AFTER 5 EPOCHS END DATE", T2ls8.date())
+print("AFTER 15 EPOCHS END DATE", T2ls8.date())
 
 passes = []  #Create an empty list
 for i in x:
     landsat8 = datetime.datetime(myyear, mymonth, mydate)
-    # print(landsat8)
     next_pass = landsat8 + datetime.timedelta( days= -i)
-    passes.append(next_pass) # Stores the passes to the passes[] list
+    passes.append(next_pass) # Stores the passes to the list
 
     if next_pass <= T2ls8:
         print("Day", i, next_pass.strftime('%A, %b %d'))
-        # print(predictor.get_next_pass(JUJA, next_pass, max_elevation_gt=30))
 
-
-print("\n\n")
-print(len(passes))
-print("Initial Pass = ", landsat8) # type: ignore
-print("Last Pass = ", passes[5])
-landsat8 = passes[5]
-print("Updated Initial Pass = ", landsat8)
 # ----------------------------------------------------------------------------------
 
 
@@ -80,16 +72,15 @@ x2 = range(0, (satelitte2())*11, satelitte2()) # type: ignore
 print("\n\n")
 print("SENTINEL 2A")
 print("TODAYS DATE", T.date())
-print("AFTER 5 EPOCHS END DATE", T2s2A.date())
+print("AFTER 11 EPOCHS END DATE", T2s2A.date())
 
 
 passes2 = []  #Create an empty list
 for j in x2:
-    sentinel2A = datetime.datetime(2023, 7, 17)
+    sentinel2A = datetime.datetime(2023, 7, 27)
     next_pass2 = sentinel2A + datetime.timedelta(days = j)
     passes2.append(next_pass) # type: ignore
 
     if next_pass2 <= T2s2A:
         print("Day", j, next_pass2.strftime('%A, %b %d'))
-        # print(predictor.get_next_pass(JUJA, next_pass2, max_elevation_gt=30))
 
